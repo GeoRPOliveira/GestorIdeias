c44de1598012feca923aca7ac37d551219015567
GeoRPOliveira <geovanna.oliveira@germinare.org.br>
2025-11-10 09:22:00 -0300

    header pronta

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,7 +50,11 @@ const authController = {
         return res.status(400).send("Email ou senha incorretos.");
       }
 
-      req.session.userId = user._id;
+      req.session.user = {
+        id: user._id,
+        name: user.name,
+        username: user.username
+      };
 
       res.redirect("/ideas");
     } catch (err) {
