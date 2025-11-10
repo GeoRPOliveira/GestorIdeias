9f50844801ece30b97f4fc185b525ff7d9951eee
cruzanab <ana.cruz@germinare.org.br>
2025-11-10 08:58:03 -0300

    Ajustando sistema de votos no back

--- models/Idea.js
+++ models/Idea.js
@@ -1,14 +1,27 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
 
 const ideaSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now },
-  votes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-const Idea = mongoose.model("Idea", ideaSchema);
-export default Idea;
+module.exports = mongoose.model('Idea', ideaSchema);
